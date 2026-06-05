a39ccb3c1254526dca4eb16c90452a90c269c935
kaitohuy <huynguyendoan0305@gmail.com>
2026-06-05 17:17:27 +0700

    Add GitHub Actions deployment

--- src/main/java/com/kaitohuy/chiabill/config/SecurityConfig.java
+++ src/main/java/com/kaitohuy/chiabill/config/SecurityConfig.java
@@ -4,7 +4,9 @@ import org.springframework.http.HttpMethod;
 import com.kaitohuy.chiabill.security.JwtFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.*;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -19,8 +21,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-                .cors(org.springframework.security.config.Customizer.withDefaults())
-                .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
